6835e2361e2c9d98cbeababa5431ad5d43746f8c
Caitlin Conway <coolcac@gmail.com>
2020-09-28 16:14:28 -0500

    home logo green on hover

--- group_flask_starter/client/src/App.js
+++ group_flask_starter/client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
 import UserList from './components/UsersList';
 import Logo from "./robinhood-logomark-white.png"
 import SearchBar from './components/SearchBar'
+import greenLogo from "./robinhood-logomark-green.png"
+
 
 function App() {
 
@@ -11,11 +13,20 @@ function App() {
         <div className="nav-bar">
         <nav>
             <ul className="nav-list">
-                <li>
-                  <NavLink to="/" activeclass="active"><img
+                <li className="home-logo-li">
+                  <NavLink to="/" activeclass="active">
+                  <img
                   className="home-logo"
                   src={Logo}
-                  /></NavLink></li>
+                  alt=""
+                  />
+                  <img
+                  className="green-home-logo"
+                  src={greenLogo}
+                  alt=""
+                  hidden
+                  />
+                  </NavLink></li>
                 <li className="search"><SearchBar></SearchBar></li>
                 <li><NavLink to="/users" activeclass="active">Account</NavLink></li>
                 <li><NavLink to="/" activeclass="active">Portfolio</NavLink></li>
