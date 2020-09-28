10a74eeb8295a61c40b22b31a70554bcc909a253
Nick Mallas <nmallas23@gmail.com>
2020-09-28 18:33:34 -0500

    chart displaying on page

--- group_flask_starter/client/src/components/StockPage.js
+++ group_flask_starter/client/src/components/StockPage.js
@@ -1,12 +1,15 @@
-import React, {useEffect} from "react"
+import React, {useEffect, useState} from "react"
+import {LineChart, XAxis, YAxis, Tooltip, ResponsiveContainer, Line} from "recharts"
 
 export default function StockPage(props) {
     let ticker = props.match.params.stockId;
+    const [stockData, setStockData] = useState("");
     useEffect(()=> {
         async function getStock() {
             const res = await fetch(`/api/stocks/${ticker}`);
             const data = await res.json();
             console.log(data);
+            setStockData(data.values)
             return data;
         }
         getStock()
@@ -14,5 +17,17 @@ export default function StockPage(props) {
 
 
 
-    return `${props.match.params.stockId}`;
+    return (
+        <div className="stock-chart">
+            <ResponsiveContainer width="50%" height={500} >
+                <LineChart data={stockData}>
+                    <XAxis dataKey="time" />
+                    <YAxis dataKey="closing" />
+                    <Tooltip />
+                    {/* <CartesianGrid stroke="#f5f5f5" /> */}
+                    <Line type="monotone" dataKey="closing" stroke="#88DEFB" yAxisId={0} dot={false}/>
+                </LineChart>
+            </ResponsiveContainer>
+        </div>
+    )
 }
--- group_flask_starter/starter_app/app/api/stock_routes.py
+++ group_flask_starter/starter_app/app/api/stock_routes.py
@@ -14,4 +14,4 @@ def stock(stockId):
     r = requests.get(f'https://finnhub.io/api/v1/stock/candle?symbol={stockId.upper()}&resolution=D&from=1577836800&to={timestamp}&token={api_key}')
     json = r.json()
     print(json["c"], json["t"])
-    return {"values": list(zip(json["c"], json["t"]))}
+    return {"values": [{"closing": a, "time": b} for (a, b) in (zip(json["c"], json["t"]))]}
