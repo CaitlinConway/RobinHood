17beacbde2cad7a752db64f19e12e98c44344ebd
Caitlin Conway <coolcac@gmail.com>
2020-09-28 15:50:03 -0500

    nav bar added

--- group_flask_starter/client/src/App.js
+++ group_flask_starter/client/src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
-
 import UserList from './components/UsersList';
-
+import Logo from "./robinhood-logomark-white.png"
+import SearchBar from './components/SearchBar'
 
 function App() {
 
   return (
     <BrowserRouter>
+        <div className="nav-bar">
         <nav>
-            <ul>
-                <li><NavLink to="/" activeclass="active">Home</NavLink></li>
-                <li><NavLink to="/users" activeclass="active">Users</NavLink></li>
+            <ul className="nav-list">
+                <li>
+                  <NavLink to="/" activeclass="active"><img
+                  className="home-logo"
+                  src={Logo}
+                  /></NavLink></li>
+                <li className="search"><SearchBar></SearchBar></li>
+                <li><NavLink to="/users" activeclass="active">Account</NavLink></li>
+                <li><NavLink to="/" activeclass="active">Portfolio</NavLink></li>
             </ul>
         </nav>
+        </div>
         <Switch>
             <Route path="/users">
                 <UserList />
             </Route>
 
             <Route path="/">
-                <h1>My Home Page</h1>
             </Route>
         </Switch>
     </BrowserRouter>
