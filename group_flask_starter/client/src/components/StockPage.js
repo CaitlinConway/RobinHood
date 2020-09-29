36907019e1abc16e6c218e150dbd65afece84c8e
Nick Mallas <nmallas23@gmail.com>
2020-09-29 18:27:35 -0500

    more-stocks

--- group_flask_starter/client/src/components/StockPage.js
+++ group_flask_starter/client/src/components/StockPage.js
@@ -6,6 +6,7 @@ export default function StockPage(props) {
     let ticker = props.match.params.stockId;
     const [stockData, setStockData] = useState("");
     const [stockPrice, setStockPrice] = useState("0");
+    const [companyData, setCompanyData] = useState({})
     useEffect(()=> {
         async function getStock() {
             const res = await fetch(`/api/stocks/${ticker}`);
@@ -13,16 +14,27 @@ export default function StockPage(props) {
             setStockData(data.values)
             return data;
         }
+
         async function getCurrentPrice() {
             const res = await fetch(`/api/stocks/current/${ticker}`);
             if(res.ok) {
                 const data = await res.json()
                 setStockPrice(data.values.c)
             }
-
         }
+
+        async function getProfile() {
+            const res = await fetch(`/api/stocks/profile/${ticker}`);
+            if(res.ok) {
+                const data = await res.json()
+                setCompanyData(data.values)
+            }
+        }
+
         getStock()
         getCurrentPrice()
+        getProfile()
+
     }, [ticker])
 
     const hidePrice = e => {
@@ -37,7 +49,7 @@ export default function StockPage(props) {
 
     const showTooltipData = (data) => {
         if ( data?.payload && typeof data?.payload[0] != 'undefined') {
-        return (<StockPrice first={stockData[0]?.closing} price={data.payload[0].payload.closing} name={ticker}/>)
+        return (<StockPrice first={stockData[0]?.closing} price={data.payload[0].payload.closing} name={companyData.name}/>)
         }
     }
 
@@ -47,7 +59,7 @@ export default function StockPage(props) {
             <div className="stock-chart-container" >
                 <div className="stock-chart">
                     <div className="stock-price-container">
-                        <div className="stock-name">{ticker}</div>
+                        <div className="stock-name">{companyData.name}</div>
                         <div className="stock-price" id="current-price">{"$" + stockPrice}</div>
                         {/* <div className="stock-return">{`+$${stockData[0].closing - stockData[stockData.length - 1].closing} (+106.48%) Past Year`}</div> */}
                     </div>
@@ -56,7 +68,8 @@ export default function StockPage(props) {
                             <XAxis dataKey="time"/>
                             <YAxis dataKey="closing" domain={["datamin", "auto"]} hide={true}/>
                             <Tooltip content={showTooltipData} position={{"x": 25, "y": 0}} animationDuration={2500}/>
-                            <Line type="monotone" dataKey="closing" stroke="#03C805" strokeWidth={1.8} yAxisId={0} dot={false}/>
+                            <Line stroke={stockPrice > stockData[0].closing ? "#03C805" : "#FF5103"}
+                                  strokeWidth={1.8} yAxisId={0} dot={false} type="monotone" dataKey="closing" />
                         </LineChart>
                     </ResponsiveContainer>
                 </div>
