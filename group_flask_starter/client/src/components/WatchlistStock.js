6e26123fb003b28a2e47b246df0dd9673ff19d83
Caitlin Conway <coolcac@gmail.com>
2020-09-29 17:32:04 -0500

    stock watchlist fetches current price

--- group_flask_starter/client/src/components/WatchlistStock.js
+++ group_flask_starter/client/src/components/WatchlistStock.js
@@ -2,12 +2,33 @@ import React, {useEffect, useState} from "react";
 import {LineChart, XAxis, YAxis, Tooltip, ResponsiveContainer, Line} from "recharts";
 import StockPrice from "./StockPrice";
 
-export default function StockPage({stock}) {
+export default function WatchListStock({stock}) {
+  const [stockData, setStockData] = useState("");
+  const [stockPrice, setStockPrice] = useState("0");
+    useEffect(()=> {
+        async function getStock() {
+            const res = await fetch(`/api/stocks/${stock}`);
+            const data = await res.json();
+            setStockData(data.values)
+            return data;
+        }
+        async function getCurrentPrice() {
+            const res = await fetch(`/api/stocks/current/${stock}`);
+            if(res.ok) {
+                const data = await res.json()
+                setStockPrice(data.values.c)
+            }
+
+        }
+        getStock()
+        getCurrentPrice()
+    }, [stock])
+
     return (
     <>
     <div className = 'individual-watchlist-div'>
       <p>{stock}</p>
-      <p>$100</p>
+      <p>{stockPrice}</p>
       </div>
     </>
     )
