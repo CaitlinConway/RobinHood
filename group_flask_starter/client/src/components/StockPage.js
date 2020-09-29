267c53ee1349303e4027936876845419af6b97f8
Nick Mallas <nmallas23@gmail.com>
2020-09-29 16:24:29 -0500

    Chart price updates on hover

--- group_flask_starter/client/src/components/StockPage.js
+++ group_flask_starter/client/src/components/StockPage.js
@@ -5,31 +5,51 @@ import StockPrice from "./StockPrice";
 export default function StockPage(props) {
     let ticker = props.match.params.stockId;
     const [stockData, setStockData] = useState("");
+    const [stockPrice, setStockPrice] = useState(0);
     useEffect(()=> {
         async function getStock() {
             const res = await fetch(`/api/stocks/${ticker}`);
             const data = await res.json();
-            console.log(data);
             setStockData(data.values)
             return data;
         }
+        async function getCurrentPrice() {
+            const res = await fetch(`/api/stocks/current/${ticker}`);
+            if(res.ok) {
+                const data = await res.json()
+                setStockPrice(data.values.c)
+            }
+
+        }
         getStock()
+        getCurrentPrice()
     }, [ticker])
 
-
-
+    const showTooltipData = (data) => {
+        if ( data?.payload && typeof data?.payload[0] != 'undefined') {
+        //   setStockPrice(data.payload[0].payload.closing);
+        return (<StockPrice price={data.payload[0].payload.closing} name={ticker}/>)
+        }
+    }
     return (
-        <div className="stock-chart-container">
-            <div className="stock-chart">
-                {/* <StockPrice/> */}
-                <ResponsiveContainer width="100%" height={500}>
-                    <LineChart data={stockData}>
-                        <XAxis dataKey="time"/>
-                        <YAxis dataKey="closing" domain={["datamin", "auto"]}/>
-                        <Tooltip position={{ x: 100, y: 0 }}/>
-                        <Line type="monotone" dataKey="closing" stroke="#88DEFB" strokeWidth={1.8} yAxisId={0} dot={false}/>
-                    </LineChart>
-                </ResponsiveContainer>
+        !stockData ? null :
+        <div className="stock-page">
+            <div className="stock-chart-container">
+                <div className="stock-chart">
+                    <div className="stock-price-container">
+                        <div className="stock-name">{"amzn"}</div>
+                        <div className="stock-price stock-price-current">{"$" + "0"}</div>
+                        {/* <div className="stock-return">{`+$${stockData[0].closing - stockData[stockData.length - 1].closing} (+106.48%) Past Year`}</div> */}
+                    </div>
+                    <ResponsiveContainer width="100%" height={500}>
+                        <LineChart data={stockData}>
+                            <XAxis dataKey="time"/>
+                            <YAxis dataKey="closing" domain={["datamin", "auto"]}/>
+                            <Tooltip content={showTooltipData} position={{"x": 100, "y": 0}}/>
+                            <Line type="monotone" dataKey="closing" stroke="#03C805" strokeWidth={1.8} yAxisId={0} dot={false}/>
+                        </LineChart>
+                    </ResponsiveContainer>
+                </div>
             </div>
         </div>
     )
--- group_flask_starter/starter_app/app/api/stock_routes.py
+++ group_flask_starter/starter_app/app/api/stock_routes.py
@@ -13,9 +13,16 @@ def stock(stockId):
     timestamp = int(time.time())
     r = requests.get(f'https://finnhub.io/api/v1/stock/candle?symbol={stockId.upper()}&resolution=D&from=1577836800&to={timestamp}&token={api_key}')
     json = r.json()
-    print({"values": [{"closing": round(a, 2),
-                        "time": datetime.datetime.fromtimestamp(int(b)).strftime("%b-%d-%Y")}
-                       for (a, b) in (zip(json["c"], json["t"]))]})
     return {"values": [{"closing": round(a, 2),
                         "time": datetime.datetime.fromtimestamp(int(b)).strftime("%b-%d-%Y")}
                        for (a, b) in (zip(json["c"], json["t"]))]}
+
+
+@stock_routes.route("/current/<stockId>")
+def getCurrent(stockId):
+    print(stockId)
+    api_key = os.environ.get("FINHUB_API_KEY")
+    r = requests.get(f'https://finnhub.io/api/v1/quote?symbol={stockId.upper()}&token={api_key}')
+    res = r.json()
+    print(res)
+    return({"values": res})
