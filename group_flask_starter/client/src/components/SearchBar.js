c4d5421970859a683f81e131fa871cb2d72544da
Dustin Schulte <dschulte1982@hotmail.com>
2020-10-01 12:34:52 -0700

    Search bar mostly complete; need Linking and case-sensitivity

--- group_flask_starter/client/src/components/SearchBar.js
+++ group_flask_starter/client/src/components/SearchBar.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { connect } from "react-redux";
 import { getSearch } from "../store/stockReducer";
+import { Link } from 'react-router-dom';
 
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       search: "",
+      label: [{ticker: "AMZN", name: "Amazon"}, {ticker: "AAPL", name: "Apple"}, {ticker: "FB", name: "Facebook"}]
     }
   };
 
@@ -16,13 +18,32 @@ class SearchBar extends React.Component {
     const searchList = document.getElementById("search-list");
     if (e.target.value) {
     searchList.removeAttribute("hidden");
-      for (let i = 0; i < tickers.length; i++) {
-      if (tickers[i]) {
-        console.log(tickers[i].charAt(0));
+    searchList.style.display = "flex";
+      name = []
+      labels = []
+      // tickers.forEach(ticker => {
+      //   for (const key in ticker) {
+      //     const checkStr = e.target.value;
+      //     if (key === "name" && ticker[key].startsWith(checkStr)) {
+      //     name.push(ticker[key])
+      //     }
+      //     if (key === "ticker" && ticker[key].startsWith(checkStr)) {
+      //       labels.push(ticker[key])
+      //     }
+      this.state.label.forEach(ticker => {
+        for (const key in ticker) {
+          const checkStr = e.target.value;
+          if ((key === "name" || key === "ticker") && (ticker[key].startsWith(checkStr)) && !(name.includes(ticker)) ) {
+            name.push(ticker);
+          }
+          // if (key === "ticker" && ticker[key].startsWith(checkStr)) {
+          //   labels.push(ticker[key])
+          // }
       }
-    }
+      });
     } else {
       searchList.setAttribute("hidden", "true")
+      searchList.style.display = "none";
     }
   };
 
@@ -34,6 +55,10 @@ class SearchBar extends React.Component {
 
   render() {
     const { search } = this.state;
+    const pagePath = {
+      pathname: "/test",
+      key: Math.random(),
+    }
 
     return (
       <div className="search-bar-container">
@@ -48,16 +73,21 @@ class SearchBar extends React.Component {
             />
         </div>
         <div id="search-list" className="search-bar-list" hidden>
-          <section id="section-1">Test #1</section>
-          <section id="section-2">Test #2</section>
+          <section id="section-1">{name.map(array => (<ul className="search-ul" key="stock"><Link to={pagePath}>{array.ticker}</Link></ul>))}</section>
+          {/* <section id="section-1">{name.map(stock => (<ul key={stock}><a href={`stocks/${stock}`}>{stock}</a></ul>))}</section> */}
+          <section id="section-1">{name.map(array => (<ul className="search-ul" key="stock"><Link to={`stocks/${array.ticker}`}>{array.name}</Link></ul>))}</section>
+          {/* <section id="section-2">{labels.map(stock => (<ul key={stock}><a href={`stocks/${stock}`}>{stock}</a></ul>))}</section> */}
         </div>
       </div>
     );
   }
 }
 
-// const tickers = [{ticker: "AAPL", name: "Apple"}, {ticker: "AMZN", name: "AMAZON"}]
-const tickers = ["Apple", "Cat"]
+
+let name = []
+let labels = []
+
+const tickers = [{ticker: "AMZN", name: "Amazon"}, {ticker: "AAPL", name: "Apple"}, {ticker: "FB", name: "Facebook"}, {ticker: "F", name: "Ford"}, {ticker: "GOOG", name: "Google"}]
 
 const mapStateToProps = (state) => ({
   search: state.search,
