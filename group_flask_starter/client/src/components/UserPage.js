51d6804b44f5688684099b9c2728d089a1269442
Dustin Schulte <dschulte1982@hotmail.com>
2020-10-02 16:07:55 -0700

    User profile page in-progress

--- group_flask_starter/client/src/components/UserPage.js
+++ group_flask_starter/client/src/components/UserPage.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { BrowserRouter,  NavLink, Redirect } from 'react-router-dom';
+import Logo from "../robinhood-logomark-white.png"
+import greenLogo from "../robinhood-logomark-green.png"
+import SearchBar from './SearchBar'
+import AccountDropDown from './AccountDropDown'
 import {connect} from 'react-redux'
 import {logOut} from '../store/authReducer'
 import { getStocklist } from '../store/stockReducer';
@@ -11,22 +16,60 @@ class UserPage extends React.Component{
       stocklist: this.props.stocklist
     }
   }
-  logoutButtonHandle = (e) =>{
-    e.preventDefault();
-    this.props.logOut();
-  }
+
   componentDidMount() {
     this.props.getStocklist(this.props.auth.id);
   }
+
+  hideAccount = (e) =>{
+    e.preventDefault();
+    let accountLi = document.getElementById('account-drop-down-li');
+    accountLi.style.display = "none";
+  }
+  showAccount = (e) =>{
+    e.preventDefault();
+    let accountLi = document.getElementById('account-drop-down-li');
+    accountLi.style.display = "flex";
+  }
+
   render(){
     return(
-    <>
-
-      <div>
-        <button id={'logout-button'} onClick={this.logoutButtonHandle}>Logout</button>
+      <div id="user-profile-page">
+        <div className="nav-bar">
+          <nav>
+            <ul className="nav-list">
+                <li className="home-logo-li">
+                  <NavLink to="/" activeclass="active">
+                  <img
+                  className="home-logo"
+                  src={Logo}
+                  alt=""
+                  />
+                  <img
+                  className="green-home-logo"
+                  src={greenLogo}
+                  alt=""
+                  hidden
+                  />
+                  </NavLink></li>
+                <li className="search"><SearchBar></SearchBar></li>
+                <li><button onClick={this.showAccount} activeclass="active" className='user-account-button'>Account</button></li>
+                <li id={'account-drop-down-li'} hidden><AccountDropDown user={this.state.user}></AccountDropDown></li>
+                <li><NavLink to="/" activeclass="active" className = 'portfolio-button'>Portfolio</NavLink></li>
+            </ul>
+          </nav>
+          </div>
+          <div id="profile-master-container">
+            <div id="profile-user-name">{this.state.user.firstName} {this.state.user.lastName}</div>
+            <div id="profile-portfolio-value" className="profile-portfolio-header">Total Portfolio Value
+              <div id="profile-user-balance">--Portfolio Balance Will Go Here--</div>
+            </div>
+            <div id="profile-cash-balance-container">
+              <div id="profile-cash-balance-header">Cash</div>
+              <div id="profile-cash-balance">{this.state.user.balance}</div>
+            </div>
+        </div>
       </div>
-
-    </>
     )
   }
 }
