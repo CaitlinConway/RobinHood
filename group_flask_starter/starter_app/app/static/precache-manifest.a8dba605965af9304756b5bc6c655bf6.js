d6dafeabfe7b36c67674966deb2fcbf42f656c99
Nick Mallas <nmallas23@gmail.com>
2020-10-02 18:09:21 -0500

    deployed to heroku

---
+++ group_flask_starter/starter_app/app/static/precache-manifest.a8dba605965af9304756b5bc6c655bf6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08eed960fb2de091d0984b82b91e7d26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db4e2441c9b3c84a5895",
+    "url": "/static/css/main.359033b1.chunk.css"
+  },
+  {
+    "revision": "3d7cc2edf7cd0a11c631",
+    "url": "/static/js/2.ad84a861.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/static/js/2.ad84a861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db4e2441c9b3c84a5895",
+    "url": "/static/js/main.b83e4ce5.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "077524feaf946ff370d407271ec97b8b",
+    "url": "/static/media/Vector_search_icon.077524fe.svg"
+  },
+  {
+    "revision": "087f6edbc2216b4916ad51cd22217260",
+    "url": "/static/media/batman-icon-png-12037.087f6edb.png"
+  },
+  {
+    "revision": "8ed3179282de9b6300cb3bddcc6b14df",
+    "url": "/static/media/green batman.8ed31792.png"
+  }
+]);
